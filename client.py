84e8392ae04b777925a93b63b611996a335640e6
timbrueck <tim.brueck@outlook.de>
2021-04-29 17:17:08 +0200

    Added a client and a server file and pasted existing code

---
+++ client.py
@@ -0,0 +1,55 @@
+import socket
+import random
+from threading import Thread
+from datetime import datetime
+from colorama import Fore, init, Back
+import os
+
+init()
+
+
+colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX, 
+    Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX, 
+    Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX, 
+    Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW
+]
+
+
+client_color = random.choice(colors)
+
+SERVER_HOST = "localhost"
+SERVER_PORT = 5002 
+
+separator_token = "<SEP>" 
+
+
+s = socket.socket()
+print(f"[*] Connecting to {SERVER_HOST}:{SERVER_PORT}...")
+
+s.connect((SERVER_HOST, SERVER_PORT))
+print("[+] Connected.")
+name = input("Enter your name: ")
+
+def listen_for_messages():
+    while True:
+        message = s.recv(1024).decode()
+        print("\n" + message)
+
+t = Thread(target=listen_for_messages)
+t.daemon = True
+t.start()
+
+while True:
+    # message
+    to_send =  input()
+    # quit program
+    if to_send.lower() == 'q':
+        print("Hope you had a great chat")
+        break
+    elif to_send == "clear":
+        os.system("cls")
+    date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
+    to_send = f"{client_color}[{date_now}] {name}{separator_token}{to_send}{Fore.RESET}"
+    s.send(to_send.encode())
+
+s.close()
